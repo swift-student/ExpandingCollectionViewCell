232e2a2dc1fe95f428989a10c8f205a0d57dd24b
Shawn Gee <shawngee@gmail.com>
2020-09-30 11:36:58 -0400

    remove diffable data source extension

--- ExpandingCollectionViewCell/PeopleViewController.swift
+++ ExpandingCollectionViewCell/PeopleViewController.swift
@@ -105,16 +105,6 @@ extension PeopleViewController: UICollectionViewDelegate {
     }
 }
 
-extension UICollectionViewDiffableDataSource {
-    /// Reapplies the current snapshot to the data source, animating the differences.
-    /// - Parameters:
-    ///   - completion: A closure to be called on completion of reapplying the snapshot.
-    func refresh(completion: (() -> Void)? = nil) {
-        self.apply(self.snapshot(), animatingDifferences: true, completion: completion)
-    }
-}
-
-
 // MARK: - SwiftUI Previews
 
 struct PeopleVCWrapper: UIViewRepresentable {
